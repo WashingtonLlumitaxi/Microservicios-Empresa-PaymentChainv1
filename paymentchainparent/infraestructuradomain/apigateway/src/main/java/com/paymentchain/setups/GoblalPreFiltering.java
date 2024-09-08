e18be2691518d1d73e38b11fc1873dbe388c282b
David <davidllumitaxi67@gmail.com>
2024-09-08 00:21:24 -0500

    Prefilter and PostFilet APIGATEWAY

--- paymentchainparent/infraestructuradomain/apigateway/src/main/java/com/paymentchain/setups/GoblalPreFiltering.java
+++ paymentchainparent/infraestructuradomain/apigateway/src/main/java/com/paymentchain/setups/GoblalPreFiltering.java
@@ -4,8 +4,10 @@
  */
 package com.paymentchain.setups;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
@@ -13,12 +15,17 @@ import reactor.core.publisher.Mono;
  *
  * @author DELL
  */
+
+@Slf4j
+@Component
 public class GoblalPreFiltering implements GlobalFilter{
 
+    //Pre-filter
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-        
+        //throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        log.info("Global prefilter executed");
+        return chain.filter(exchange);
     }
     
 }
